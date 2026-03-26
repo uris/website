7fed97d9df5ec1b48d09d77201ed7472feaa4905
uris <urisvirott@yahoo.com>
2026-03-25 23:34:25 -0700

    test apple slice library

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
+import { ThemeProvider } from '@apple-pie/slice';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
 
 const root = document.getElementById('root');
 if (root) {
 	createRoot(root).render(
 		<StrictMode>
-			<App />
+			<ThemeProvider system global>
+				<App />
+			</ThemeProvider>
 		</StrictMode>,
 	);
 }
